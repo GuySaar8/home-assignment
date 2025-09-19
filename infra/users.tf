22a4279fd619d6e0f7ba114b78e1827c3d364e23
Guy Saar <guysaar8@gmail.com>
2025-09-19 12:27:10 -0500

    chore: terraform user

--- infra/users.tf
+++ infra/users.tf
@@ -2,19 +2,35 @@ resource "aws_iam_user" "deployment_user" {
   name = "deployment-user"
 }
 
+# Attach managed policy for EKS cluster management
 resource "aws_iam_user_policy_attachment" "eks_policy" {
   user       = aws_iam_user.deployment_user.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+# Attach managed policy for full ECR access
 resource "aws_iam_user_policy_attachment" "ecr_policy" {
   user       = aws_iam_user.deployment_user.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
-resource "aws_iam_user_policy_attachment" "eks_describe_policy" {
-  user       = aws_iam_user.deployment_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# Custom inline policy for DescribeCluster
+resource "aws_iam_user_policy" "eks_describe_policy" {
+  name = "deployment-user-eks-describecluster"
+  user = aws_iam_user.deployment_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "eks:DescribeCluster"
+        ]
+        Resource = "arn:aws:eks:us-east-1:793480251750:cluster/main-eks"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_access_key" "deployment_user_key" {
