c1bf4e2cf737ab8bef5beda4de052e5ab56204c8
Guy Saar <guysaar8@gmail.com>
2025-09-20 15:47:41 -0500

    refactor: replace test assertions with module definitions for VPC and EKS

--- infra/tests/main.tf
+++ infra/tests/main.tf
@@ -15,31 +15,38 @@ terraform {
   }
 }
 
-resource "test_assertions" "vpc" {
-  component = "vpc"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
-  equal "cidr_block" {
-    description = "VPC has correct CIDR"
-    got         = module.vpc.vpc_cidr_block
-    want        = "10.0.0.0/16"
-  }
+  name = "test-vpc"
+  cidr = "10.0.0.0/16"
 
-  check "subnet_count" {
-    description = "VPC has correct number of subnets"
-    condition   = length(module.vpc.private_subnets) > 0 && length(module.vpc.public_subnets) > 0
-  }
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
-resource "test_assertions" "eks" {
-  component = "eks"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-  check "cluster_version" {
-    description = "EKS cluster version is supported"
-    condition   = module.eks.cluster_version >= "1.24"
-  }
+  cluster_name    = "test-eks"
+  cluster_version = "1.27"
 
-  check "node_groups" {
-    description = "EKS has node groups configured"
-    condition   = length(module.eks.eks_managed_node_groups) > 0
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    test = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+    }
   }
 }
